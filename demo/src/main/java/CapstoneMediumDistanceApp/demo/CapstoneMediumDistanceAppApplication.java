bb039d9598d58fba330ac2ea4fc17b0cba821ae8
jaeseok <jssjchl@naver.com>
2021-03-25 11:27:16 +0900

    Initial commit

---
+++ demo/src/main/java/CapstoneMediumDistanceApp/demo/CapstoneMediumDistanceAppApplication.java
@@ -0,0 +1,12 @@
+package CapstoneMediumDistanceApp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneMediumDistanceAppApplication {
+
+	public static void main(String[] args) { SpringApplication.run(CapstoneMediumDistanceAppApplication.class, args);
+	}
+
+}
